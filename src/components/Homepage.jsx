2910407a1ba053700cff5ac92b0e648c5a4fd5e7
Mohsin <mohsin-ahmed01@outlook.com>
2024-02-06 10:08:22 +0000

    implement changes in response to tutor feedback

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,14 +1,8 @@
 import { Link } from "react-router-dom";
-import Header from "./Header";
-import Navbar from "./Navbar";
 
 export default function Homepage() {
   return (
     <>
-      <Header></Header>
-
-      <Navbar></Navbar>
-
       <h2>Welcome!</h2>
 
       <p>
